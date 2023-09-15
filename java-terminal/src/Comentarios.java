9e276694d52a0e3a71465f358aee7d55aa769236
karolineterra <terra.karoline1@gmail.com>
2023-09-15 15:54:04 -0300

    scanner

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,18 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        //Olá eu sou um comentário em uma única linha
+        /*Olá
+         * Eu sou um comentário
+         * em várias
+         * linhas
+         */
+
+    }
+
+    /**
+     * Estas duas estelinhas acima identificam que você está elaborando um comentário a nível de documentação.
+     */
+    public void metodo(){
+
+    }
+}
