3172acdd5928453d13dce92dad5508b2c1267504
karolineterra <terra.karoline1@gmail.com>
2023-09-21 14:58:00 -0300

    condicionais

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,12 +1,15 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0; 
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 26.0;
 
         if(valorSolicitado < saldo){
-            saldo = saldo - valorSolicitado;
+            saldo -= valorSolicitado;
+            System.out.println("Novo saldo: " + saldo);
+        } else {
+            System.out.println("Não há saldo suficiente em conta");
         }
 
-        System.out.println(saldo);
+        
     }
 }
