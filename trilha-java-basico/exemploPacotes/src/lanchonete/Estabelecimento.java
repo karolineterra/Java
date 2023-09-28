87d2e485dfa4cf46e5f309f5c2de9f015c48ef76
karolineterra <terra.karoline1@gmail.com>
2023-09-28 15:38:42 -0300

    visibilidade dos recursos

---
+++ trilha-java-basico/exemploPacotes/src/lanchonete/Estabelecimento.java
@@ -0,0 +1,31 @@
+package lanchonete;
+
+import lanchonete.area.cliente.Cliente;
+import lanchonete.atendimento.Atendente;
+import lanchonete.atendimento.cozinha.*;
+
+
+public class Estabelecimento {
+	public static void main(String[] args) {
+		Cozinheiro cozinheiro = new Cozinheiro();
+		
+		//ações que estabelecimento precisa ter ciência
+		cozinheiro.adicionarSucoNoBalcao();;
+		cozinheiro.adicionarLancheNoBalcao();
+		cozinheiro.adicionarComboNoBalcao();
+		
+		Almoxarife almoxarife = new Almoxarife();
+		//ações que não precisam estarem disponíveis para toda a aplicação
+		
+		Atendente atendente = new Atendente();
+		atendente.receberPagamento();
+		atendente.servindoMesa();
+		
+		Cliente cliente = new Cliente();
+		cliente.escolherLanche();
+		cliente.fazerPedido();
+		cliente.pagarConta();
+		
+		
+	}
+}
