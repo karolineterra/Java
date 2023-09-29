c6d6a8cb6f6242e0ff18f6560268aeb3131c6967
karolineterra <terra.karoline1@gmail.com>
2023-09-29 16:03:00 -0300

    encapsulamento

---
+++ msn-messenger-poo/src/ComputadorPedrinho.java
@@ -0,0 +1,7 @@
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+        MSNMessenger msn = new MSNMessenger();
+        msn.enviarMensagem();
+        msn.receberMensagem();
+    }
+}
