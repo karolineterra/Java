5f553052dd189b5914d240c052839ea31342c394
karolineterra <terra.karoline1@gmail.com>
2023-10-16 16:23:27 -0300

    pesquisa na collection set

---
+++ collections-java/src/main/set/Pesquisa/Contato.java
@@ -0,0 +1,44 @@
+package main.set.Pesquisa;
+
+import java.util.Objects;
+
+public class Contato {
+    // atributos
+    private String nome;
+    private int numero;
+
+    public Contato(String nome, int numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(!(obj instanceof Contato contato)) return false;
+        return Objects.equals(getNome(), contato.getNome());
+    }
+
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "Contato: " + nome + ", " + "Numero: " + numero;
+    }
+}
